4fdfdcbc899a2794187f6bf1ccadbd8861feac20
GreenNugget <naomi.snchz99@gmail.com>
2024-07-31 13:19:37 -0600

    :tada: begin a project!

---
+++ src/main/java/com/naomi/test/JavaProjectApplication.java
@@ -0,0 +1,14 @@
+package com.naomi.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+		//System.out.println("HOLA MUNDO");
+	}
+
+}
